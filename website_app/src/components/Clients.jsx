6d647cbe4ca88d61afdd5aeeef4ba4472f37cbe7
Erik Chan <chanerik1000@gmail.com>
2022-08-24 21:43:34 -0700

    Added Hover Effects to Clients

--- website_app/src/components/Clients.jsx
+++ website_app/src/components/Clients.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import {clients} from '../constants'
+import styles from '../style.js'
 
-export const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+export const Clients = () => (
+  <section className={`${styles.flexCenter} myy-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+          <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain hover:opacity-50 duration-300 ease-out"/>
+        </div>
+      ))}
+    </div>
+  </section>
+)
