1f2731bf6d849c86b9dde482c745541a6fc954b6
Erik Chan <chanerik1000@gmail.com>
2022-08-24 21:25:54 -0700

    80%

--- website_app/src/components/Testimonials.jsx
+++ website_app/src/components/Testimonials.jsx
@@ -4,7 +4,7 @@ import styles from '../style.js'
 
 export const Testimonials = () => (
   <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
-    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient"/>
+    <div className="absolute z-[0] w-[80%] h-[80%] -right-[50%] rounded-full blue__gradient"/>
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
       <h1 className={styles.heading2}>What people are <br className='sm:block hidden'/> saying about us</h1>
